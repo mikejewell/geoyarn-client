375737920805acdc00c1065a0483656a29842d54
Charlie Hargood <charliehargood@gmail.com>
2012-09-17 15:05:43 +0100

    StoryList Initial

--- src/uk/ac/soton/ecs/geoyarn/StoriesActivity.java
+++ src/uk/ac/soton/ecs/geoyarn/StoriesActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 public class StoriesActivity extends Activity {
@@ -26,7 +27,7 @@ public class StoriesActivity extends Activity {
         
         for(Story s:stories){
         	Button storyButton = new Button(this);
-        	storyButton.setText(s.title);
+        	storyButton.setText(s.getTitle());
         	storyButton.setTag(s);
         	storyButton.setOnClickListener(new View.OnClickListener(){
     			public void onClick(View v) {
@@ -45,16 +46,28 @@ public class StoriesActivity extends Activity {
     	
     	ArrayList<Story> tempStories = new ArrayList<Story>();
     	
-    	tempStories.add(new Story("Story 1",1));
-    	tempStories.add(new Story("Story 2",2));
-    	tempStories.add(new Story("Story 3",3));
+    	Story story1 = new Story();
+    	story1.setTitle("Story 1");
+    	story1.setId(1);
+    	
+    	Story story2 = new Story();
+    	story2.setTitle("Story 2");
+    	story2.setId(2);
+    	
+    	Story story3 = new Story();
+    	story3.setTitle("Story 3");
+    	story3.setId(3);
+    	
+    	tempStories.add(story1);
+    	tempStories.add(story2);
+    	tempStories.add(story3);
     	
     	return tempStories;    	
     }
     	
     
     public void startStory(Story s){
-    	
+    	Toast.makeText(this, "Starting story: "+s.getTitle(), Toast.LENGTH_SHORT).show();
     }
     
     
