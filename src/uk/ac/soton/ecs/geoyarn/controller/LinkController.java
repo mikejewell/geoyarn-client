075fdb21f306065c96d54e224f21690c76fe34aa
Mike Jewell <mjewell@gmail.com>
2012-09-17 15:33:55 +0100

    BaseCont/StoryCont - gets urls

--- src/uk/ac/soton/ecs/geoyarn/controller/LinkController.java
+++ src/uk/ac/soton/ecs/geoyarn/controller/LinkController.java
@@ -1,4 +1,5 @@
 package uk.ac.soton.ecs.geoyarn.controller;
 
 public class LinkController {
+	
 }
--- src/uk/ac/soton/ecs/geoyarn/controller/StoryController.java
+++ src/uk/ac/soton/ecs/geoyarn/controller/StoryController.java
@@ -1,13 +1,26 @@
 package uk.ac.soton.ecs.geoyarn.controller;
 
 import java.util.ArrayList;
-import java.util.List;
+
+import org.json.JSONArray;
 
 import uk.ac.soton.ecs.geoyarn.model.Story;
+import android.util.Log;
 
-public class StoryController {
-	public List<Story> getStories() {
-		List<Story> stories = new ArrayList<Story>();
+public class StoryController extends BaseController {
+	
+	private static final String TAG = "StoryController";
+	
+	public ArrayList<Story> getStories() {
+		ArrayList<Story> stories = new ArrayList<Story>();
+		try {
+			String storyText = this.getURL("http://lab.thecollectedmike.com/yarn/stories.json");
+			JSONArray storyJSON = new JSONArray(storyText);
+			Log.v(TAG, "json=" + storyJSON);
+		} catch (Exception e) {
+
+		}
 		return stories;
 	}
+
 }
