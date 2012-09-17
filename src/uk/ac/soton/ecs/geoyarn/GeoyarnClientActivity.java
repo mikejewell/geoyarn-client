9e78f96a8475b4b13aa222f074f16968ba55d2f6
Charlie Hargood <charliehargood@gmail.com>
2012-09-17 12:47:04 +0100

    New project

---
+++ src/uk/ac/soton/ecs/geoyarn/GeoyarnClientActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.soton.ecs.geoyarn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GeoyarnClientActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
