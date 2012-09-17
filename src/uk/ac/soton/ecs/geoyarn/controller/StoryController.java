c21edfb82ed1218b855a3bd4648ad5487035aea4
Mike Jewell <mjewell@gmail.com>
2012-09-17 15:43:41 +0100

    * Gets stories from url!

--- src/uk/ac/soton/ecs/geoyarn/controller/StoryController.java
+++ src/uk/ac/soton/ecs/geoyarn/controller/StoryController.java
@@ -3,6 +3,7 @@ package uk.ac.soton.ecs.geoyarn.controller;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import uk.ac.soton.ecs.geoyarn.model.Story;
 import android.util.Log;
@@ -15,10 +16,20 @@ public class StoryController extends BaseController {
 		ArrayList<Story> stories = new ArrayList<Story>();
 		try {
 			String storyText = this.getURL("http://lab.thecollectedmike.com/yarn/stories.json");
-			JSONArray storyJSON = new JSONArray(storyText);
-			Log.v(TAG, "json=" + storyJSON);
+			JSONArray storiesJSON = new JSONArray(storyText);
+			
+			for(int i=0; i<storiesJSON.length(); i++)
+			{
+				JSONObject storyJSON = storiesJSON.getJSONObject(i);
+				Story story = new Story();
+				story.setTitle(storyJSON.getString("title"));
+				story.setId(storyJSON.getInt("id"));
+				stories.add(story);
+			}
+			
 		} catch (Exception e) {
 
+			Log.e(TAG, e.getMessage());
 		}
 		return stories;
 	}
